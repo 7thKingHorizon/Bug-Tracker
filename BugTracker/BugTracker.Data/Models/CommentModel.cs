32c3906abb45a84eeb0618229307b1c77ddf0886
carlos cuevas <cunning.cricket@gmail.com>
2020-06-28 15:12:08 -0700

    Comment Model Shell Created

---
+++ BugTracker/BugTracker.Data/Models/CommentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data.Models
+{
+    class CommentModel
+    {
+        public string CommentId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string CreatedBy { get; set; }
+
+    }
+}
