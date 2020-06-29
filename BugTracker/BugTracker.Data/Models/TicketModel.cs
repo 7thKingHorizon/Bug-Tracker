ab61f3f2a83e91ff59254c69b635438822224627
carlos cuevas <cunning.cricket@gmail.com>
2020-06-29 13:27:17 -0700

    Created Ticket Model Shell

---
+++ BugTracker/BugTracker.Data/Models/TicketModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data.Models
+{
+    class TicketModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Detail { get; set; }
+
+        public List<UserModel> Assignments { get; set; }
+
+        public List<UserModel> Followers { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public UserModel CreatedBy { get; set; }
+
+    }
+}
